a5c6ddb8b140304e38da6880135dd5a6e01b4a91
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-02-04 12:47:50 -0700

    the current weather stuff will generate on click, local storage almost working

--- script.js
+++ script.js
@@ -1,38 +1,95 @@
-//VARIABLES/////////////////////////////////////////////////////////////////
-var currentForecastBox;
-var predictionFiveDayBox;
-var searchHistoryBox;
+
+
 //FUNCTIONS////////////////////////////////////////////////////////////////
+
+function storeSearchHistory() {
+
+    console.log("drawSearchHistory is working");
+
+    const inputBoxValue = document.getElementById("input-box");
+    let previousCities = []; 
+    var searchHistoryBox = document.getElementById("search-history");
+    const cityName = inputBoxValue.value.trim();
+    if (cityName){
+        previousCities.push(cityName);
+        localStorage.setItem("cities", JSON.stringify(previousCities));
+    } else {
+        alert("Information required. Try again.");
+    }
+
+}
 
 //the following function will render the search history with clickable previous cities
 function drawSearchHistory() {
 
-    searchHistoryBox = document.getElementById("");
-
 }
+
 //the following function will render the current weather for the searched city
-function drawCurrentForecast(date, temp, wind, humidity, uv_index) {
+function drawCurrentForecast(city_name, date, temp, wind, humidity, uv_index) {
 
-    currentForecastBox = document.getElementById("current-weather");
+    var currentForecastBox = document.getElementById("current-weather");
+    console.log("drawCurrentForecast is working");
 
+    //generates the City Name when search is clicked
+    const paraCity = document.createElement("p");
+    const nodeCity = document.createTextNode("Current weather conditions for: " + city_name);
+    paraCity.appendChild(nodeCity);
+    document.getElementById("current-weather").appendChild(paraCity);
+
+    //generates the Date when search clicked
+    const paraDate = document.createElement("p");
+    const nodeDate = document.createTextNode("Date: " + date);
+    paraDate.appendChild(nodeDate);
+    document.getElementById("current-weather").appendChild(paraDate);
+
+    //generates the Temp when search clicked
+    const paraTemp = document.createElement("p");
+    const nodeTemp = document.createTextNode("Temp: " + temp);
+    paraTemp.appendChild(nodeTemp);
+    document.getElementById("current-weather").appendChild(paraTemp);
+
+    //generates the Wind when search clicked
+    const paraWind = document.createElement("p");
+    const nodeWind = document.createTextNode("Wind: " + wind);
+    paraWind.appendChild(nodeWind);
+    document.getElementById("current-weather").appendChild(paraWind);
+
+    //generates the Humidity when search clicked
+    const paraHumidity = document.createElement("p");
+    const nodeHumidity = document.createTextNode("Humidity: " + humidity);
+    paraHumidity.appendChild(nodeHumidity);
+    document.getElementById("current-weather").appendChild(paraHumidity);
+
+    //generates the uv_index when search clicked
+    const paraUVIndex = document.createElement("p");
+    const nodeUVIndex = document.createTextNode("UV Index: " + uv_index);
+    paraUVIndex.appendChild(nodeUVIndex);
+    document.getElementById("current-weather").appendChild(paraUVIndex);
 
 }
 
 //the following function will render the five day forecast for the searched city
 function drawFiveDayForecast() {
 
-    predictionFiveDayBox = document.getElementById("five-day-forecast");
+    var predictionFiveDayBox = document.getElementById("five-day-forecast");
+    console.log("drawFiveDayForecast is working");
 
 }
 
 //the following function will be performed when seach is clicked (event listener at bottom in the logic portion)
 function search() {
-    drawCurrentForecast("02/15/30", 30, 31, 32, 33);
-    drawFiveDayForecast();
-    drawSearchHistory();
+    console.log("i have been clicked");
+    drawCurrentForecast();
+    storeSearchHistory();
 }
 
 //LOGIC////////////////////////////////////////////////////////////////////
+//VARIABLES/////////////////////////////////////////////////////////////////
+$(document).ready(function () {
 
+    const start_btn = document.getElementById('search-button');
 
+    start_btn.addEventListener('click', search);
+
+});
 
